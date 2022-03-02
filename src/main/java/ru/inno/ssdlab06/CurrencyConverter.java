ec181dc8acf3cca1e0f5c569379a793de9cd7c6c
G-Akleh <98668965+G-Akleh@users.noreply.github.com>
2022-03-02 17:29:51 +0300

    Update CurrencyConverter.java

--- src/main/java/ru/inno/ssdlab06/CurrencyConverter.java
+++ src/main/java/ru/inno/ssdlab06/CurrencyConverter.java
@@ -2,5 +2,17 @@ package ru.inno.ssdlab06;
 
 public class CurrencyConverter {
 
-  //TODO your code here
+  private double ExRate;
+
+    public void setExRate(double ExRate){
+        this.ExRate = ExRate;
+    }
+
+    public double getExRate() {
+        return ExRate;
+    }
+
+    public double convert(double cur1, double cur2){
+        return cur2 = cur1 * ExRate;
+    }
 }
