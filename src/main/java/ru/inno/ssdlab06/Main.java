807e86d0836147238c52a8b26ee7e133ec90d6c6
G-Akleh <98668965+G-Akleh@users.noreply.github.com>
2022-03-02 17:31:14 +0300

    Update Main.java

--- src/main/java/ru/inno/ssdlab06/Main.java
+++ src/main/java/ru/inno/ssdlab06/Main.java
@@ -3,7 +3,9 @@ package ru.inno.ssdlab06;
 public class Main {
 
 	public static void main(final String[] args) {
-		System.out.println("Hello world!");
+		//10 options of currency for the use to choose from
+		//Use the currency converter class
+		//Output the program
 	}
 
 }
